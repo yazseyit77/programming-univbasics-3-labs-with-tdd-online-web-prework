7b3728b3f9e1f8e42265c19b4f9d7822008c9985
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-30 03:38:11 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,5 @@
 # Add your variables here
-fisrt_number = 80
-second_number = 2
-sum = first_number + second_number
+fisrt_number = "80"
+second_number = "2"
+
+sum = "#{first_number} + #{second_number}"
